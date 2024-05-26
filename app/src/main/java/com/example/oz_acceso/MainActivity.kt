c6f01768c2b38d329a0f7918cf33e270e72e14c9
Sebastian "Bastian" Guerrero <gr.jsebastian@gmail.com>
2024-05-26 13:47:12 -0500

    Create README.md

--- app/src/main/java/com/example/oz_acceso/MainActivity.kt
+++ app/src/main/java/com/example/oz_acceso/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.example.oz_acceso
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.oz_acceso.ui.login.LoginActivity
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate Called")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.welcome_button)
-        rollButton.text = "Welcome"
-        rollButton.setOnClickListener{
-            layoutInflater
+        findViewById<Button>(R.id.welcomeButton)
+        .setOnClickListener {
+            // Handler code here.
+            val intent = Intent (this, LoginActivity::class.java)
+            startActivity(intent);
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -23,5 +29,35 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
     }
 }
